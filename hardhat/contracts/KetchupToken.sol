dd1e5da721685db06aa6b10f47b248504c45fb55
zerotoxicity <31730313+zerotoxicity@users.noreply.github.com>
2022-09-14 21:00:48 +0800

    initial commit

---
+++ hardhat/contracts/KetchupToken.sol
@@ -0,0 +1,7 @@
+pragma solidity 0.8.16;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract KetchupToken is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+}
