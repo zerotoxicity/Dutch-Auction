eea7a4dc3df70bc0e86d96f203ee5aff2d7393d2
zerotoxicity <31730313+zerotoxicity@users.noreply.github.com>
2022-09-28 21:53:36 +0800

    Added unit test for Ketchup token

---
+++ hardhat/test/KetchupToken.test.js
@@ -0,0 +1,41 @@
+const { ethers, upgrades, waffle } = require("hardhat");
+const { expect } = require("chai");
+
+describe("💰 Ketchup Token", function () {
+  let ketchupContract;
+  let deployer;
+  let accounts;
+
+  beforeEach(async function () {
+    accounts = await ethers.getSigners();
+    deployer = accounts[0];
+    const KetchupTokenV1 = await ethers.getContractFactory("KetchupTokenV1");
+    ketchupContract = await upgrades.deployProxy(
+      KetchupTokenV1,
+      ["Ketchup", "KCH"],
+      {
+        kind: "uups",
+      }
+    );
+
+    await ketchupContract.transferOwnership(accounts[1].address);
+    await ketchupContract.connect(accounts[1]).fundAuction();
+  });
+
+  it("👨 Owner should be changed to accounts[1]", async function () {
+    expect(await ketchupContract.owner()).to.equals(accounts[1].address);
+  });
+
+  it("🔨 Tokens should be minted ", async function () {
+    expect(await ketchupContract.balanceOf(accounts[1].address)).to.equal(
+      BigInt(1e20)
+    );
+  });
+
+  it("🔥 Remaining token should be burnt", async function () {
+    await ketchupContract.connect(accounts[1]).burnRemainingToken(BigInt(1e18));
+    expect(await ketchupContract.balanceOf(accounts[1].address)).to.equal(
+      BigInt(1e20 - 1e18)
+    );
+  });
+});
